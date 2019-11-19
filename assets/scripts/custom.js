cad1cf3e85b81bdd7496037e1049fe8a2a883d69
Laura P <laura.pociuniene@gmail.com>
2019-11-19 09:52:33 +0200

    Hide/show gallery fixes, image transitions

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -20,8 +20,22 @@ burger.addEventListener("click", function(){
     menu.classList.toggle("show");
 });
 
+
 let sort = document.querySelector(".galery-toggle");
-let galeryMenu = document.querySelector(".galery-block");
-sort.addEventListener("click", function(){
-    galeryMenu.classList.toggle("show");
+let galleryMenu = document.querySelector(".photo-carousel");
+let showNum = 3;
+sort.addEventListener("click", function() {
+  if (showNum < galleryMenu.children.length) {
+    showNum = showNum + 3;
+    for (let i = 0; i < showNum; i++) {
+      galleryMenu.children[i].classList.add("show");
+      galleryMenu.children[i].children[0].classList.add("enableTransition");
+    }
+  } else {
+    for (let i = 0; i < showNum; i++) {
+      galleryMenu.children[i].classList.remove("show");
+      galleryMenu.children[i].children[0].classList.remove("enableTransition");
+    }
+    showNum = 3;
+  }
 });
